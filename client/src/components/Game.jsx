0ee12085c54cdbf724fee0b7222f4b696664ce72
Anders Myrman <amyhrman@gmail.com>
2023-10-20 23:53:41 +0200

    Game GUI Fixes #1

--- client/src/components/Game.jsx
+++ client/src/components/Game.jsx
@@ -1,10 +1,27 @@
+import GameBoard from "./GameBoard";
+import Keyboard from "./Keyboard";
+import "./Game.css";
+
 function Game() {
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Backspace") {
+      // Handle backspace key press
+    } else if (event.key === "Enter") {
+      // Handle enter key press
+    } else {
+      // Handle letter key press
+    }
+  };
+
   return (
     <div>
       <h1>Wordle</h1>
       <p>Guess the secret word in 6 tries or less!</p>
-      <div>
-      </div>
+
+      <GameBoard numRows={6} />
+
+      <Keyboard handleKeyPress={handleKeyPress} />
     </div>
   );
 }
