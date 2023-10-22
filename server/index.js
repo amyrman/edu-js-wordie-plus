7406e899e1ec739dcc86969a4db6086ed5afddf9
Anders Myrman <amyhrman@gmail.com>
2023-10-22 20:43:45 +0200

    Add routes for start game and guessing

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
-const express = require("express");
+import express from "express";
+import bodyParser from "body-parser";
+import { getCorrectWord } from "./utils/feedback.js";
 const port = process.env.port || 3001;
 const app = express();
+
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello from backend!" });
@@ -12,6 +16,20 @@ app.get("/highscore", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.json({ message: "Hello from backend!" });
+});
+
+let correctWord;
+
+app.post("/start", (req, res) => {
+  const { numLetters, repeatableLetters } = req.body;
+  correctWord = getCorrectWord(numLetters, repeatableLetters);
+  res.sendStatus(200);
+});
+
+app.post('/guess', (req, res) => {
+  const { guessWord } = req.body;
+  const checkedLetters = feedback(guessWord, correctWord);
+  res.json({ checkedLetters });
 });
 
 app.listen(port, () => {
