451f9f2c4f64f46b809745688591384bf2b52a99
Anders Myrman <amyhrman@gmail.com>
2023-11-14 11:54:53 +0100

    clean up

--- client/src/components/GameBoard.jsx
+++ client/src/components/GameBoard.jsx
@@ -4,65 +4,52 @@ import { sendGuess } from "../services/api";
 import "../styles/GameBoard.css";
 
 export default function GameBoard({
-    desiredWordLength,
-    allowRepeatedLetters,
-    maxGuess,
+    desiredWordLength
 }) {
-    // keysArray is used to store and render keys in the UI
     const [keysArray, setKeysArray] = useState([]);
-    // currentCell is used to keep track of which cell should render output (letters, feedback)
-    const [currentCell, setCurrentCell] = useState(-1);
-    const [currentRow, setCurrentRow] = useState(1);
     const [feedbackArray, setFeedbackArray] = useState([]);
 
-    // guessWord is used to store the completed keysArray as a string -- when user presses enter, it is sent to the backend (where it's compared to the correct word)
-    // const guessWord;
+    const gameOver = () => {
+        // Logic for game over goes here...
+        console.log("Game over, you won!");
+    };
 
-    // just use two rows instead and keep track of number of guesses
-    // by using state
-    // just use row id=1 for typing in guesses and when sent, move it to
-    // row id=0
-    // this also means: remove maxGuess from for loop and just set it to "2"...this should maybe be configurable though...but through maxRows
+    const handleGuess = async (guessWord) => {
+        try {
+            const responseData = await sendGuess({ guessWord: guessWord });
+            console.log("Response data:", responseData);
 
-    // if correctWord then output feedback "You found the secret word!", and prompt user to input name, and other needed data is tracked and stored either in db or in session, fetched and output in the highscore component...
+            // Assuming responseData.checkedLetters contains the feedback for the guessed word
+            const checkedLetters = responseData.checkedLetters;
+            console.log(responseData.checkedLetters);
 
-    // // maxGuess could also be used and gameover when guesses === maxGuess. then feed back: "You are out of guesses! Try again! And being able to push "Yeah, give it to me!", "Restart random game", add option to set min max words length for randomizer
+            if (checkedLetters === null) {
+                throw new Error("checkedLetters is null");
+            }
 
-    // todo: add user registration, XP, achievements, real-time competition
+            if (!Array.isArray(checkedLetters)) {
+                throw new Error("checkedLetters is not an array");
+            }
 
-    // todo: set default options to only choosing word length and repeated, and every thing else to "Advanced options"
+            if (checkedLetters.every((item) => item.result === "correct")) {
+                gameOver();
+            } else {
+                console.log("Not all letters are correct");
+            }
 
-    const generateGrid = () => {
-      const rows = [];
-      for (let i = 0; i < 2; i++) {
-          const cells = [];
-          for (let j = 0; j < desiredWordLength; j++) {
-              if (i === 0) {
-                  // Display feedback in row 0
-                  cells.push(
-                      <Cell key={j} id={j} className={feedbackArray[j]?.className}>
-                          {feedbackArray[j]?.key}
-                      </Cell>
-                  );
-              } else {
-                  // Allow typing in row 1
-                  cells.push(
-                      <Cell key={j} id={j}>
-                          {keysArray[j]}
-                      </Cell>
-                  );
-              }
-          }
-          rows.push(
-              <div key={i} id={i} className="row">
-                  {cells}
-              </div>
-          );
-      }
-      return rows;
-  };
+            setFeedbackArray(
+                checkedLetters.map((item) => {
+                    return {
+                        key: item.letter,
+                        className: item.result,
+                    };
+                })
+            );
+        } catch (error) {
+            console.error("Error in handleGuess:", error);
+        }
+    };
 
-    // To register keyboard input, and decide what happens when we press keys, we assign this to "onKeyDown" (which is a reserved prop in React)
     const handleKeyDown = (event) => {
         const newKey = event.key;
         const guessWord = keysArray.join("");
@@ -89,43 +76,41 @@ export default function GameBoard({
                 }
                 break;
         }
-
-        async function handleGuess(guessWord) {
-            try {
-                const responseData = await sendGuess({ guessWord: guessWord });
-                console.log("Response data:", responseData);
-
-                // Assuming responseData.checkedLetters contains the feedback for the guessed word
-                const checkedLetters = responseData.checkedLetters;
-                console.log(checkedLetters);
-
-                // Update feedbackArray with the letters and results from checkedLetters
-                const newFeedbackArray = checkedLetters.map((item) => ({
-                    key: item.letter,
-                    className: item.result,
-                }));
-
-                console.log("New feedback array:", newFeedbackArray);
-
-                setFeedbackArray(newFeedbackArray);
-            } catch (error) {
-                console.error("Error in handleGuess:", error);
-            }
-        }
     };
 
-    // setCurrentCell((currentCell) => {
-    //     // problem might be that it always increments?
-    //     const nextCell = currentCell + 1;
-    //     // desiredWordLength does not start on 0?
-    //     if (currentCell === desiredWordLength) {
-    //         console.log("currentCell reached dWL: ", desiredWordLength);
-    //         return desiredWordLength;
-    //     } else if (currentCell < nextCell) {
-    //         console.log("currentCell in else: ", nextCell);
-    //         return nextCell;
-    //     }
-    // });
+    const generateGrid = () => {
+        const rows = [];
+        for (let i = 0; i < 2; i++) {
+            const cells = [];
+            for (let j = 0; j < desiredWordLength; j++) {
+                if (i === 0) {
+                    // Display feedback in row 0
+                    cells.push(
+                        <Cell
+                            key={j}
+                            id={j}
+                            className={feedbackArray[j]?.className}
+                        >
+                            {feedbackArray[j]?.key}
+                        </Cell>
+                    );
+                } else {
+                    // Allow typing in row 1
+                    cells.push(
+                        <Cell key={j} id={j}>
+                            {keysArray[j]}
+                        </Cell>
+                    );
+                }
+            }
+            rows.push(
+                <div key={i} id={i} className="row">
+                    {cells}
+                </div>
+            );
+        }
+        return rows;
+    };
 
     return (
         <>
